0d9549d9d2d96a4701e6623087abf853a5ac7554
Suryansh Sinha <suryanshsinha.dev@gmail.com>
2025-09-09 23:36:07 +0600

    add regression

--- streamlit_regression.py
+++ streamlit_regression.py
@@ -25,7 +25,7 @@ age = st.slider('Age', 18, 92)
 balance = st.number_input('Balance')
 credit_score = st.number_input('Credit Score')
 exited = st.slider('Exited', 0, 1)
-estimated_salary = st.number_input('Estimated Salary')
+
 tenure = st.slider('Tenure', 0, 10)
 num_of_products = st.slider('Number of Products', 1, 4)
 has_cr_card = st.selectbox('Has Credit Card', [0,1])
